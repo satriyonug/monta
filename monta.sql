f147f329e1fbae38482dfbbb4a1d1dcdd931d55b
Satriyo Nugroho <satriyo.nug97@gmail.com>
2019-03-12 14:43:32 -0700

    update sql

--- monta.sql
+++ monta.sql
@@ -1,8 +1,8 @@
 /*
-SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.5.5-10.1.33-MariaDB : Database - monta
+SQLyog Professional v13.1.1 (64 bit)
+MySQL - 10.1.37-MariaDB : Database - monta
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -29,7 +29,38 @@ CREATE TABLE `tb_dosen` (
 
 /*Data for the table `tb_dosen` */
 
-insert  into `tb_dosen`(`id_dosen`,`nip`,`nama_dosen`) values (1,'198410162008121002','Radityo Anggoro S.Kom, M.Sc'),(2,'198608232015041004','Abdul Munif, S.Kom., M.Sc.'),(3,'198701032014041001','Rizky Januar Akbar, S.Kom., M.Eng.'),(4,'196505181992031003','Dr.tech. Ir. R.V.HARI GINARDI, M.Sc.'),(5,'1994201912088','Kelly Rossa Sungkono, S.Pd., M,Pd.');
+insert  into `tb_dosen`(`id_dosen`,`nip`,`nama_dosen`) values 
+(1,'198410162008121002','Radityo Anggoro S.Kom, M.Sc'),
+(2,'198608232015041004','Abdul Munif, S.Kom., M.Sc.'),
+(3,'198701032014041001','Rizky Januar Akbar, S.Kom., M.Eng.'),
+(4,'196505181992031003','Dr.tech. Ir. R.V.HARI GINARDI, M.Sc.'),
+(5,'1994201912088','Kelly Rossa Sungkono, S.Pd., M,Pd.');
+
+/*Table structure for table `tb_judul` */
+
+DROP TABLE IF EXISTS `tb_judul`;
+
+CREATE TABLE `tb_judul` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_mhs` varchar(100) NOT NULL,
+  `nrp_mhs` varchar(14) NOT NULL,
+  `rmk` varchar(4) NOT NULL,
+  `judul_ta` text NOT NULL,
+  `abstrak` text NOT NULL,
+  `pembimbing_ta` varchar(100) NOT NULL,
+  `nip` varchar(30) NOT NULL,
+  `catatan` text,
+  `status` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tb_judul` */
+
+insert  into `tb_judul`(`id`,`nama_mhs`,`nrp_mhs`,`rmk`,`judul_ta`,`abstrak`,`pembimbing_ta`,`nip`,`catatan`,`status`,`created_at`,`updated_at`) values 
+(2,'Nabilah Zaki Lismia','05111540000020','IGS','Simulasi Berbicara dalam Bahasa Asing Berbasis Realitas Virtual Menggunakan Speech Recognition, Chatbot, dan Teknologi Google Daydream','Simulasi Berbicara dalam Bahasa Asing Berbasis Realitas Virtual Menggunakan Speech Recognition, Chatbot, dan Teknologi Google Daydream','Abdul Munif, S.Kom., M.Sc.','198608232015041004',NULL,0,'2019-03-13 03:55:34','2019-03-13 03:55:34'),
+(3,'Satriyo Nugroho','05111540000034','MI','Virtual Assistant Chatbot pada aplikasi Gifood.id Menggunakan Speech Recognition dengan Algoritma Natural Language Processing','Virtual Assistant Chatbot pada aplikasi Gifood.id Menggunakan Speech Recognition dengan Algoritma Natural Language Processing dan Cousin Similarity','Dr.tech. Ir. R.V.HARI GINARDI, M.Sc.','196505181992031003','Ganti Judul',1,'2019-03-13 03:57:55','2019-03-13 04:10:49');
 
 /*Table structure for table `tb_proposal` */
 
@@ -56,11 +87,14 @@ CREATE TABLE `tb_proposal` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id_proposal`),
   UNIQUE KEY `prefix` (`prefix`,`id_proposal`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 
 /*Data for the table `tb_proposal` */
 
-insert  into `tb_proposal`(`prefix`,`id_proposal`,`nama_mhs`,`nrp`,`rmk`,`judul_ta`,`abstrak_ta`,`kata_kunci_ta`,`pembimbing1_ta`,`nip1`,`pembimbing2_ta`,`nip2`,`proposal_ta`,`referensi_ta`,`revisi`,`status`,`created_at`,`updated_at`) values ('TA',15,'Nabilah Zaki Lismia','05111540000020','IGS','Simulasi Berbicara dalam Bahasa Asing Berbasis Realitas Virtual Menggunakan Speech Recognition, Chatbot, dan Teknologi Google Daydream','Simulasi Berbicara dalam Bahasa Asing Berbasis Realitas Virtual Menggunakan Speech Recognition, Chatbot, dan Teknologi Google Daydream\r\n','daydream','Rizky Januar Akbar, S.Kom., M.Eng.','198701032014041001','Abdul Munif, S.Kom., M.Sc.','198608232015041004','lembar_pengesahan.pdf',NULL,NULL,'Mengajukan Dosbing','2019-03-08 13:20:22','2019-03-09 20:54:52'),('TA',16,'SHAFIRA AISYAH RAHMADHANI','05111540000098','RPL','Rancang Bangun Aplikasi Katalog Bersama Perpustakaan ITS Terintegrasi Berbasis Web','Rancang Bangun Aplikasi Katalog Bersama Perpustakaan ITS Terintegrasi Berbasis Web','katalog','Rizky Januar Akbar, S.Kom., M.Eng.','198701032014041001','Radityo Anggoro S.Kom, M.Sc','198410162008121002','',NULL,NULL,'Mengajukan Dosbing',NULL,'2019-03-08 19:05:13'),('TA',17,'Alfindio Muhammad Abdallah','05111540000164','MI','Sentiment Analysis pada Tweet berbahasa Indonesia menggunakan fastText dengan metode klasifikasi Naïve Bayes, Decision Tree, dan Random Forest','Sentiment Analysis pada Tweet berbahasa Indonesia menggunakan fastText dengan metode klasifikasi Naïve Bayes, Decision Tree, dan Random Forest\r\n','Naive Bayes','Abdul Munif, S.Kom., M.Sc.','198608232015041004','',NULL,'Proposal TA - Satriyo Nugroho - 05111540000034.pdf','',NULL,'Mendaftar','2019-03-08 16:00:42','2019-03-08 21:15:18'),('TA',29,'Alvin Mudhoffar','05111540000062','RPL','Implementasi Otentikasi Single-Factor dan Multi-Factor Berbasis Protokol WebAuthn di Aplikasi myITS Single Sign-On','Implementasi Otentikasi Single-Factor dan Multi-Factor Berbasis Protokol WebAuthn di Aplikasi myITS Single Sign-On','SSO','Rizky Januar Akbar, S.Kom., M.Eng.','198701032014041001','Abdul Munif, S.Kom., M.Sc.','198608232015041004','Proposal_TA_-_Satriyo_Nugroho_-_05111540000034.pdf',NULL,NULL,'Mengajukan Dosbing','2019-03-09 15:32:19','2019-03-09 15:34:09'),('TA',30,'Satriyo Nugroho','05111540000034','MI','Virtual Assistant Chatbot pada aplikasi Gifood.id Menggunakan Speech Recognition dengan Algoritma Natural Language Processing','Virtual Assistant Chatbot pada aplikasi Gifood.id Menggunakan Speech Recognition dengan Algoritma Natural Language Processing','NLP','Dr.tech. Ir. R.V.HARI GINARDI, M.Sc.','196505181992031003','Kelly Rossa Sungkono, S.Pd., M,Pd.','1994201912088','Proposal_TA_-_Satriyo_Nugroho_-_051115400000341.pdf',NULL,'Menambah pertanyaan chatbot','Mendaftar','2019-03-11 15:05:54','2019-03-11 16:20:41');
+insert  into `tb_proposal`(`prefix`,`id_proposal`,`nama_mhs`,`nrp`,`rmk`,`judul_ta`,`abstrak_ta`,`kata_kunci_ta`,`pembimbing1_ta`,`nip1`,`pembimbing2_ta`,`nip2`,`proposal_ta`,`referensi_ta`,`revisi`,`status`,`created_at`,`updated_at`) values 
+('TA',17,'Alfindio Muhammad Abdallah','05111540000164','MI','Sentiment Analysis pada Tweet berbahasa Indonesia menggunakan fastText dengan metode klasifikasi Naïve Bayes, Decision Tree, dan Random Forest','Sentiment Analysis pada Tweet berbahasa Indonesia menggunakan fastText dengan metode klasifikasi Naïve Bayes, Decision Tree, dan Random Forest\r\n','Naive Bayes','Abdul Munif, S.Kom., M.Sc.','198608232015041004','',NULL,'Proposal TA - Satriyo Nugroho - 05111540000034.pdf','',NULL,'Mendaftar','2019-03-08 16:00:42','2019-03-08 21:15:18'),
+('TA',29,'Alvin Mudhoffar','05111540000062','RPL','Implementasi Otentikasi Single-Factor dan Multi-Factor Berbasis Protokol WebAuthn di Aplikasi myITS Single Sign-On','Implementasi Otentikasi Single-Factor dan Multi-Factor Berbasis Protokol WebAuthn di Aplikasi myITS Single Sign-On','SSO','Rizky Januar Akbar, S.Kom., M.Eng.','198701032014041001','Abdul Munif, S.Kom., M.Sc.','198608232015041004','Proposal_TA_-_Satriyo_Nugroho_-_05111540000034.pdf',NULL,NULL,'Mendaftar','2019-03-09 15:32:19','2019-03-09 15:34:09'),
+('TA',33,'Satriyo Nugroho','05111540000034','MI','Virtual Assistant Chatbot pada aplikasi Gifood.id Menggunakan Speech Recognition dengan Algoritma Natural Language Processing','Virtual Assistant Chatbot pada aplikasi Gifood.id Menggunakan Speech Recognition dengan Algoritma Natural Language Processing dan Cousin Similarity          ','NLP','Dr.tech. Ir. R.V.HARI GINARDI, M.Sc.',NULL,'Kelly Rossa Sungkono, S.Pd., M,Pd.','1994201912088','Proposal_TA_-_Satriyo_Nugroho_-_05111540000034.pdf',NULL,NULL,'Menunggu Sidang Proposal','2019-03-13 04:12:32','2019-03-13 04:40:35');
 
 /*Table structure for table `tb_user` */
 
@@ -77,7 +111,18 @@ CREATE TABLE `tb_user` (
 
 /*Data for the table `tb_user` */
 
-insert  into `tb_user`(`id_user`,`nama`,`departemen`,`password`,`role`) values ('05111540000020','Nabilah Zaki Lismia','Informatika','9397720251b99acaca13a1e51685e983','Mahasiswa'),('05111540000034','Satriyo Nugroho','Informatika','0d6efea15b90466e303899093a483e2d','Mahasiswa'),('05111540000062','Alvin Mudhoffar','Informatika','accdb1983f7f9eb3e72f5bec7b84db7d','Mahasiswa'),('05111540000164','Alfindio Muhammad Abdallah','Informatika','53f24a129d986e373e52dfcbaaf0c016','Mahasiswa'),('196505181992031003','Dr.tech. Ir. R.V.HARI GINARDI, M.Sc.','Informatika','6bc3ace09cd36d99f001cf746fbfdb7c','Dosen'),('198410162008121002','Radityo Anggoro S.Kom, M.Sc','Informatika','2ced6917907aa67010a5dd145cac9ecd','Kaprodi'),('198608232015041004','Abdul Munif, S.Kom., M.Sc.','Informatika','c6913604d673829aeffdf10b80a46b70','Dosen'),('198701032014041001','Rizky Januar Akbar, S.Kom., M.Eng.','Informatika','7bf9de6972cd7f6fc21729a690e541b1','Dosen'),('1994201912088','Kelly Rossa Sungkono, S.Pd., M,Pd.','Informatika','8d6cf99eee40078d14c74f7689cf34e2','Dosen'),('tim_rmk','Tim RMK Informatika','Informatika','e2fdf27972f69f4f774dbc6fd37f4e01','Rmk');
+insert  into `tb_user`(`id_user`,`nama`,`departemen`,`password`,`role`) values 
+('05111540000020','Nabilah Zaki Lismia','Informatika','9397720251b99acaca13a1e51685e983','Mahasiswa'),
+('05111540000034','Satriyo Nugroho','Informatika','0d6efea15b90466e303899093a483e2d','Mahasiswa'),
+('05111540000040','Abyan Dafa','Informatika','d8b895fbc9eef2864658eb5739f77b75','Mahasiswa'),
+('05111540000062','Alvin Mudhoffar','Informatika','accdb1983f7f9eb3e72f5bec7b84db7d','Mahasiswa'),
+('05111540000164','Alfindio Muhammad Abdallah','Informatika','53f24a129d986e373e52dfcbaaf0c016','Mahasiswa'),
+('196505181992031003','Dr.tech. Ir. R.V.HARI GINARDI, M.Sc.','Informatika','6bc3ace09cd36d99f001cf746fbfdb7c','Dosen'),
+('198410162008121002','Radityo Anggoro S.Kom, M.Sc','Informatika','2ced6917907aa67010a5dd145cac9ecd','Kaprodi'),
+('198608232015041004','Abdul Munif, S.Kom., M.Sc.','Informatika','c6913604d673829aeffdf10b80a46b70','Dosen'),
+('198701032014041001','Rizky Januar Akbar, S.Kom., M.Eng.','Informatika','7bf9de6972cd7f6fc21729a690e541b1','Dosen'),
+('1994201912088','Kelly Rossa Sungkono, S.Pd., M,Pd.','Informatika','8d6cf99eee40078d14c74f7689cf34e2','Dosen'),
+('tim_rmk','Tim RMK Informatika','Informatika','e2fdf27972f69f4f774dbc6fd37f4e01','Rmk');
 
 /*Table structure for table `tb_web` */
 
@@ -101,7 +146,8 @@ CREATE TABLE `tb_web` (
 
 /*Data for the table `tb_web` */
 
-insert  into `tb_web`(`id_web`,`nama_web`,`domain_web`,`slogan_web`,`alamat_web`,`telp_web`,`fax_web`,`email_web`,`author_web`,`deskripsi_web`,`keyword_web`,`tahun_web`) values (1,'MONTA ITS','.ID','Aplikasi Web Pengajuan Tugas Akhir / Skripsi','Ds. Maju Jaya RT 09 RW 02, Kecamatan Maju Jaya, Kabupaten Pati, Provinsi Jawa Tengah','081215409236','---','rumitkode@gmail.com','Kode Rumit','PPDB RUMIT adalah Aplikasi Pendaftaran Peserta Didik Baru Berbasis Web','Aplikasi PPDB, Web PPDB Online Free',2019);
+insert  into `tb_web`(`id_web`,`nama_web`,`domain_web`,`slogan_web`,`alamat_web`,`telp_web`,`fax_web`,`email_web`,`author_web`,`deskripsi_web`,`keyword_web`,`tahun_web`) values 
+(1,'MONTA ITS','.ID','Aplikasi Web Pengajuan Tugas Akhir / Skripsi','Ds. Maju Jaya RT 09 RW 02, Kecamatan Maju Jaya, Kabupaten Pati, Provinsi Jawa Tengah','081215409236','---','rumitkode@gmail.com','Kode Rumit','PPDB RUMIT adalah Aplikasi Pendaftaran Peserta Didik Baru Berbasis Web','Aplikasi PPDB, Web PPDB Online Free',2019);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
